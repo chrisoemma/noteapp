2b65dc73aa321a8c7b27677ecba988d984c1343c
chrisso emma <chrissoemma@gmail.com>
2019-04-21 08:28:47 +0300

    add t to softDeleted method

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ Route::get('/v1/notes', 'NotesController@allNotes');
 Route::delete('v1/notes/{id}', 'NotesController@permanentDelete'); //this on first permanent delete models
 Route::delete('v2/notes/{id}', 'NotesController@softDelete');
 Route::get('v2/notes/withsoftdelete','NotesController@notesWithSoftDelete');
-Route::get('v2/notes/softdeleted','NotesController@sofDeleted');
+Route::get('v2/notes/softdeleted','NotesController@softDeleted');
 Route::patch('/v1/notes/{id}','NotesController@restore');
 Route::patch('v2/notes/{id}','NotesController@permanentDeleteSoftDeleted');
